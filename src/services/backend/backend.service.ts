a1d63f6e28439df3949bc718c64698158f049454
bjoernkarthein <bjoern.karthein@t-online.de>
2021-08-02 21:55:35 +0200

    added multiplayer-game-list

--- src/services/backend/backend.service.ts
+++ src/services/backend/backend.service.ts
@@ -28,6 +28,10 @@ export class BackendService {
     return this.http.get<any>('/api/users/' + id);
   }
 
+  public async getRandomUser(uId: number) {
+    return await this.http.get<any>('/api/users/random/' + uId).toPromise();
+  }
+
   public createUser(user: User) {
     this.getUser(user.id).subscribe(response => {
       if (response.length > 0) {
@@ -40,17 +44,35 @@ export class BackendService {
     });
   }
 
-  public async getAttempt(quizId: string) {
-    return await this.http.get<any>('/api/webuserattempts/' + quizId).toPromise();
+  public async getQuizAttemptById(attemptId: number) {
+    return await this.http.get<any>('/api/single_player_attempts/' + attemptId).toPromise();
   }
 
-  public async createAttempt(quizId: string, attemptId: number) {
-    const res = await this.getAttempt(quizId);
+  public async getQuizAttemptsByUsername(username: string) {
+    return await this.http.get<any>('/api/single_player_attempts/user/' + username).toPromise();
+  }
+
+  public async getQuizAttemptsByQuizId(quizId: string) {
+    return await this.http.get<any>('/api/single_player_attempts/quiz/' + quizId).toPromise();
+  }
+
+  public async saveSinglePlayerAttempt(spa: SinglePlayerAttempt) {
+    const res = await this.getQuizAttemptById(spa.attemptId);
     console.log(res);
     if (res.length > 0) {
       return;
     }
-    this.http.post('/api/webuserattempts', { quizId, attemptId }, this.httpOptions).subscribe(response => {
+    this.http.post('/api/single_player_attempts', { spa }, this.httpOptions).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  public async getMultiPlayerAttemptsByCourseAndUser(courseId: string, userId: number): Promise<MultiPlayerAttempt[]> {
+    return await this.http.get<any>('/api/multi_player_attempts/course/' + courseId + '/user/' + userId).toPromise();
+  }
+
+  public async saveMultiPlayerAttempt(mpa: MultiPlayerAttempt) {
+    this.http.post('/api/multi_player_attempts', { mpa }, this.httpOptions).subscribe(response => {
       console.log(response);
     });
   }
@@ -64,4 +86,27 @@ export interface User {
   lastname?: string;
   username?: string;
   loggedIn?: boolean;
+}
+
+export interface SinglePlayerAttempt {
+  attemptId: number;
+  userId: number;
+  usename: string;
+  quizId: string;
+  quizname?: string;
+  totalPoints?: number;
+  grade?: number;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface MultiPlayerAttempt {
+  gameId?: number;
+  initiatorId: number;
+  opponentId: number;
+  courseId: string;
+  inProgress: boolean;
+  currentRound?: number;
+  nextTurnId?: number;
+  questionIndex?: number;
 }
